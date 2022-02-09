549cdacc46e158ad2fe79b4a376eac93a7e5b4ba
“shubham767676” <shubhamv.singh06@gmail.com>
2022-02-09 21:32:19 +0530

    Welcome to Gambling Simulator

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -1,27 +1,30 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
-
 public class GamblingSimulator {
 
-public static void main(String[] args) {
+	public static int stake = 100;
+	public static int bet = 1;
+	
+		public static void main(String[] args) {
 		
 		System.out.println("Welcome to Gambling Simulator");
-		int stake = 100;
-		int bet = 1;
-		int TRIAL = (int) Math.floor(Math.random() * 2);
+		
+		int RANDOM = (int) Math.floor(Math.random() * 2);
         
-		if (TRIAL == 0) {
-           stake++;
-        System.out.println("Gambler won the game ");
-       	System.out.println("Gambler having stake after bet $ " + stake);  
-        } 
+			if (RANDOM == 0) {
+			stake++;
+       		System.out.println("Gambler won the game ");
+       		System.out.println("Gambler having stake after bet $ " + stake);  
+			} 
         
-        else {
-           stake--;
+			else {
+			stake--;
        	
-           System.out.println("Gambler lose the game ");
-       	 System.out.println("Gambler having stake after bet $ " + stake);
-        }
-	}
+			System.out.println("Gambler lose the game ");
+       	 	System.out.println("Gambler having stake after bet $ " + stake);
+			}
+		
+		}
+
+
 }
