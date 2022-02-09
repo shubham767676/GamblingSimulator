5a372fb422cfcd359948e30838ca4776e31b002b
“shubham767676” <shubhamv.singh06@gmail.com>
2022-02-09 12:37:08 +0530

    Added: Bet Win or Lose

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -9,5 +9,19 @@ public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulator");
 		int stake = 100;
 		int bet = 1;
+		int TRIAL = (int) Math.floor(Math.random() * 2);
+        
+		if (TRIAL == 0) {
+           stake++;
+        System.out.println("Gambler won the game ");
+       	System.out.println("Gambler having stake after bet $ " + stake);  
+        } 
+        
+        else {
+           stake--;
+       	
+           System.out.println("Gambler lose the game ");
+       	 System.out.println("Gambler having stake after bet $ " + stake);
+        }
 	}
 }
