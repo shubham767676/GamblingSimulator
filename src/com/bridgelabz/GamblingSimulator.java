77e4345a8e608b651af46401969b4a2860bf4ee6
“shubham767676” <shubhamv.singh06@gmail.com>
2022-02-09 12:13:36 +0530

    Welcome Message

---
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class GamblingSimulator {
+
+public static void main(String[] args) {
+		
+		System.out.println("Welcome to Gambling Simulator");
+	}
+}
