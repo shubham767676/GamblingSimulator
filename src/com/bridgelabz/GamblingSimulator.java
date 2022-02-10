e7b4591b94214bf7ce7927c41b92dec6285af648
“shubham767676” <shubhamv.singh06@gmail.com>
2022-02-10 16:10:43 +0530

    Added: Win or Lose Logic

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -3,28 +3,27 @@ package com.bridgelabz;
 public class GamblingSimulator {
 
 	public static int stake = 100;
-	public static int bet = 1;
-	
-		public static void main(String[] args) {
-		
+	public final int bet = 1;
+
+	public static void main(String[] args) {
+
 		System.out.println("Welcome to Gambling Simulator");
-		
+
 		int RANDOM = (int) Math.floor(Math.random() * 2);
-        
-			if (RANDOM == 0) {
+
+		if (RANDOM == 0) {
 			stake++;
-       		System.out.println("Gambler won the game ");
-       		System.out.println("Gambler having stake after bet $ " + stake);  
-			} 
-        
-			else {
-			stake--;
-       	
-			System.out.println("Gambler lose the game ");
-       	 	System.out.println("Gambler having stake after bet $ " + stake);
-			}
-		
+			System.out.println("Gambler won the game ");
+			System.out.println("Gambler having stake after bet $ " + stake);
 		}
 
+		else {
+			stake--;
+
+			System.out.println("Gambler lose the game ");
+			System.out.println("Gambler having stake after bet $ " + stake);
+		}
+
+	}
 
 }
